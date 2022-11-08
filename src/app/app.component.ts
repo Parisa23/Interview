0f35519369ba309d1818579af28277e56f3ca6b1
Parisa Derogari <p.derogari@gmail.com>
2022-11-08 16:14:14 +0330

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'interview';
+  currentIndex!: number;
+  playList = [
+    {'src':'https://download.samplelib.com/mp4/sample-5s.mp4',
+    'poster':'https://samplelib.com/lib/preview/mp4/sample-5s.jpg'},
+
+    {'src':'https://download.samplelib.com/mp4/sample-15s.mp4',
+    'poster':'https://samplelib.com/lib/preview/mp4/sample-15s.jpg'},
+
+    {'src':'https://download.samplelib.com/mp4/sample-30s.mp4',
+    'poster':'https://samplelib.com/lib/preview/mp4/sample-30s.jpg'},
+
+    {'src':'https://download.samplelib.com/mp4/sample-15s.mp4',
+    'poster':'https://samplelib.com/lib/preview/mp4/sample-15s.jpg'},
+
+    {'src':'https://download.samplelib.com/mp4/sample-30s.mp4',
+    'poster':'https://samplelib.com/lib/preview/mp4/sample-30s.jpg'},
+    {'src':'https://download.samplelib.com/mp4/sample-5s.mp4',
+    'poster':'https://samplelib.com/lib/preview/mp4/sample-5s.jpg'},
+
+    {'src':'https://download.samplelib.com/mp4/sample-15s.mp4',
+    'poster':'https://samplelib.com/lib/preview/mp4/sample-15s.jpg'},
+
+    {'src':'https://download.samplelib.com/mp4/sample-30s.mp4',
+    'poster':'https://samplelib.com/lib/preview/mp4/sample-30s.jpg'},
+  ];
+
+
+  isPlay: boolean = false;
+
+  playPause() {
+    var myVideo: any = document.getElementById("video");
+    if (myVideo.paused) myVideo.play();
+    else myVideo.pause();
+  }
+
+
+  playVideo(i: any, flag:boolean){
+    this.currentIndex = i;
+    var video : any = document.getElementById("video");
+    if(!video.paused) video.pause();
+
+    video.poster = this.playList[i]['poster'];
+    video.src = this.playList[i]['src']
+    if(flag) video.play();
+
+
+  }
+
+  vidEnded(){
+    this.currentIndex+=1
+    if(this.currentIndex <= this.playList.length) this.playVideo(this.currentIndex, true)
+    else this.playVideo(0, true)
+
+  }
+
+  skip(value: any) {
+    let video: any = document.getElementById("video");
+    video.currentTime += value;
+  }
+
+  restart() {
+    let video: any = document.getElementById("video");
+    video.currentTime = 0;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
